3ff36b0af1c0a49c35bbc70b6bacce48048e7a2a
nickbonham <nickbonham303@gmail.com>
2020-10-04 15:25:05 -0600

    Using platformIO to upload arduino code

--- HalloweenTrickOrTreat/src/main.cpp
+++ HalloweenTrickOrTreat/src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
+#define onboard 13
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(onboard, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(onboard, LOW);
+  delay(1000);
+  digitalWrite(onboard, HIGH);
+  delay(1000);
 }
